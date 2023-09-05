ce28a1e564a125885affb873d44f0be95605c056
gilbert-ku <gilbert45ku@gmail.com>
2023-09-06 00:51:41 +0300

    complete

--- lib/grade_reports.py
+++ lib/grade_reports.py
@@ -1,4 +1,27 @@
 #!/usr/bin/env python3
 
+# def create_grade_report(student_grades):
+#     with open('lib/reports/grade_report.txt', 'w') as gr:
+#         gr.write(student_grades)
+
+# if __name__ == '__main__':
+#     student_grades = input("Student name, grade: ")
+#     create_grade_report(student_grades)
+
+
+def create_grade_report(student_grades):
+    with open('lib/reports/grade_report.txt', 'w') as gr:
+        for grade in student_grades:
+            # add '\n' to write grades on separate lines
+            gr.write(grade + '\n')
+
 if __name__ == '__main__':
-    pass
+    student_grades = []
+
+    grade = input("Student name, grade: ")
+    while grade:
+        student_grades.append(grade)
+        # end when no grade is entered
+        grade = input("Student name, grade: ")
+
+    create_grade_report(student_grades)
